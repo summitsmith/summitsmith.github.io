16df7718c98fc88995e79ae499992cef32789c5e
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-06-01 16:03:47 +0300

    Add files via upload

---
+++ donate.js
@@ -0,0 +1,3 @@
+function donate(){
+    alert("Online donations are currently unavailable, please contact us via the whatsap button in the bottom right corner or use the contact us button emails and phone numbers listed there, thanks")
+}
